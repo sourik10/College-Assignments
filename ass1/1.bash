dd73d5cba9f3320fd059cbd4ff381e74e8dac490
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-11-29 15:30:43 +0530

    Update 1.bash

--- ass1/1.bash
+++ ass1/1.bash
@@ -5,7 +5,7 @@ a=`expr $y % 4`
 b=`expr $y % 100`
 c=`expr $y % 400`
 
-if [$a -eq 0 -a $b -ne 0 -o $c -eq 0]
+if [ $a -eq 0 -a $b -ne 0 -o $c -eq 0 ]
     then 
         echo "$y is a leap year"
 else
