c5718681ee031bc5b94fbc054a18d2305078245b
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-11-29 15:25:27 +0530

    Create 1.bash

---
+++ ass1/1.bash
@@ -0,0 +1,15 @@
+clear
+echo "Enter year"
+read y
+a=`expr $y % 4`
+b=`expr $y % 100`
+c=`expr $y % 400`
+
+if [$a -eq 0 -a $b -ne 0 -o $c -eq 0]
+    then 
+        echo "$y is a leap year"
+else
+        echo "$y is not a leap year"
+
+fi
+  
