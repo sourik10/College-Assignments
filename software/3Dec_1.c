91831e3a782031231e3b5d03b7dfde07a6772332
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-05 21:03:51 +0530

    Create 3Dec_1.c

---
+++ software/3Dec_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void fact(int n){
+    if(n==0){
+        printf("0! = 1");
+    }
+    else{
+    unsigned int f=1;
+    for(int i=2;i<=n;i++){
+        f=f*i;
+    }
+    printf("%d",&f);
+    }
+    
+}
+
+int main() {
+   int m;
+   printf("Enter a number \n");
+   scanf("%d",&m);
+   (m<0)?printf("Invalid"):fact(m);
+   
+   
+    return 0;
+}
