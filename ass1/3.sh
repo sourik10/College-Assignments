c296b9cb4cb90081d9b088572eaac6ef8f89eecb
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-05 21:47:35 +0530

    Update 3.sh

--- ass1/3.sh
+++ ass1/3.sh
@@ -1,12 +1,13 @@
-#####ERROR 
+#error
 clear
 echo "Enter 1st,2nd,3rd Sides of Triangle"
 read a b c
 if [ `expr $a + $b ` -gt $c ]
 then 
-  s=`expr $a + $b + $c ` / 2
-  area=`$s \* (( $s - $a )) \* (( $s - $b))  \* (( $s - $c )) `
-  echo "Area of Triangle=$area"
+ s=`echo "scale=3; ($a+$b+$c)/2" | bc` 
+area=`echo "scale=3; sqrt($s*($s-$a)*($s-$b)*($s-$c))" | bc` 
+
+echo "Area of Triangle=$area"
 else 
   echo "Enter Correct value for Triangle"
 fi
