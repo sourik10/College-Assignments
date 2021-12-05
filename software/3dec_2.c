be6a74c9526c048858ebcf9584a4b20d301b6563
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-05 21:06:29 +0530

    Create 3dec_2.c

---
+++ software/3dec_2.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int n;
+   printf("Enter a number \n");
+   scanf("%d",&n);
+   for(int i=1;i<=n;printf("%d ",i++));
+    return 0;
+}
