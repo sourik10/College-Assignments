24ef03e0cb0767cacc2999580d7edaed3f86c0c2
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-14 02:36:05 +0530

    Update 7b_Circle.sh

--- ass1/7b_Circle.sh
+++ ass1/7b_Circle.sh
@@ -5,7 +5,7 @@ read r
 tput clear
 for((i=1;i<=360;i+=2))
 do 
-a=`echo "scale=3;3.14/180*$i"|bc|awk '{= print cos($1)}'`
+a=`echo "scale=3;3.14/180*$i"|bc|awk '{ print cos($1)}'`
 x=`echo "scale=3;$r*$a"|bc|awk '{print int($1)}'`
 b=`echo "scale=3;3.14/180*$i"|bc|awk '{print sin($1)}'`
 y=`echo "scale=3;$r*$b"|bc|awk '{print int($1)}'`
