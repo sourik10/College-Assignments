4edf6281ef1007b144d7933250037aecba98a7c9
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-09 12:18:39 +0530

    Update and rename extra.sh to database.sh

--- ass 5/database.sh
+++ ass 5/database.sh
@@ -28,16 +28,16 @@ update(){
   read roll
   echo "Enter the new marks"
   read data
-  x=`wc -l <student.txt`
+  x=`wc -l<student.txt`
   for((i=2;i<=x;i++))
   do
     y=`head -n $i student.txt | tail -n 1 | cut -d \| -f 1`
     if [ $roll -eq $y ]; then
-      val=`head -n $i student.txt | tail -n 1| cut -d\| -f 1,2`
+      val=`head -n $i student.txt | tail -n 1| cut -d \| -f 1,2`
       rm temp.txt
-      head -n $(($i-1)) student.txt >> temp.txt
+      head -n $(($i - 1)) student.txt >> temp.txt
       echo "$val|$data">>temp.txt
-      tail -n $(($x-$i)) student.txt >> temp.txt
+      tail -n $(($x - $i)) student.txt >> temp.txt
       rm student.txt
       cat temp.txt>student.txt
       break
@@ -53,6 +53,7 @@ display(){
 for ((;;)) do
   echo -e "1.Insert\n2.Delete\n3.Update\n4.Display\n5.Exit"
   read n
+
   case $n in
   1)insert;;
   2)delete;;
