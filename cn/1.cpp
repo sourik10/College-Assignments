25bad43e8a2f17e19613292bf74c9ee4a2a96f0d
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-02-13 14:16:18 +0530

    Create 1.cpp

---
+++ cn/1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll              long long int 
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+int main(){
+    fastread();
+    
+    // Code to find a data is given in Even Parity or Odd Parity
+    string code; cin>>code;
+    int ct=0;
+    for(int i=0;i<code.length();i++){
+        if(code[i]=='1'){
+            ct++;
+        }
+    }
+    if(ct%2){
+        cout<<"code is in odd parity"<<endl;
+    }
+    else cout<<"code is in even parity"<<endl;
+
+}
